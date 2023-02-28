b5522f85738705fdfb57834cee4a94bf52c65814
吴海洋 <why@vip.local>
2023-02-28 17:22:31 +0800

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 
+// 抽离回调 将会每次状态变化都执行
+const initialValue = () => {
+  console.log("initial value");
+  return 0;
+};
+
 function App() {
-  const [count, setCount] = useState<number>(0);
+  // 回调只在初始化时执行一次
+  const [count, setCount] = useState<number>(() => initialValue());
+
+  // 返回值是数组
+  const arr = useState("hello world");
+  console.log(arr);
 
   const handleCount = (num: number) => {
     setCount(count + num);
